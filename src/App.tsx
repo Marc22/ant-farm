13c5160203b59f20a00b8bbdfd6282b859ae5f18
Marc Wilson <marc22@gmail.com>
2026-01-25 16:16:48 -0500

    add dots

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import TodoApp from './components/TodoApp'
 export default function App(): JSX.Element {
   return (
     <main style={{ fontFamily: 'system-ui, sans-serif', padding: 20 }}>
-      <h1>Ant Farm Todo List.</h1>
+      <h1>Ant Farm Todo List...</h1>
       <TodoApp />
     </main>
   )
